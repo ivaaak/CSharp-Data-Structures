5fdd9bb987467427dceecdd4f867769b87b04056
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-02-28 09:53:07 +0200

    queue and singly linked list added

---
+++ 02. Linear-Data-Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,131 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+        }
+
+        private Node head;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            var node = new Node(item, this.head);
+            this.head = node;
+            this.Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            var newNode = new Node(item);
+            if(this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                var node = this.head;
+                while(node.Next != null)
+                {
+                    node = node.Next;
+                }
+                node.Next = newNode;
+            }
+            this.Count++;
+        }
+
+        public T GetFirst()
+        {
+            if(this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.head.Element;
+        }
+
+        public T GetLast()
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var node = this.head;
+
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            return node.Element;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldHead = this.head;
+            this.head = oldHead.Next;
+
+            this.Count--;
+
+            return oldHead.Element;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.head == null) //0 el
+            {
+                throw new InvalidOperationException();
+            }
+
+            var node = this.head;
+
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            var oldHead = this.head;
+            this.head = oldHead.Next;
+
+            this.Count--;
+
+            return oldHead.Element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
