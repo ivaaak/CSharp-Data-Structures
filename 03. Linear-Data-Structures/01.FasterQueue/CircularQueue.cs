6a120524eec29c994e05c440d96020ff7d9027ce
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-01 11:02:22 +0200

    added circularQueue + doublyLinkedList

---
+++ 03. Linear-Data-Structures/01.FasterQueue/CircularQueue.cs
@@ -0,0 +1,94 @@
+﻿namespace Problem01.CircularQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CircularQueue<T> : IAbstractQueue<T>
+    {
+        private T[] elements;
+        private int startIndex, endIndex;
+
+        public CircularQueue(int capacity)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public int Count { get; set; }
+
+        
+        public void Enqueue(T item)
+        {
+            if(this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+            this.elements[this.endIndex] = item;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.Count++;
+        }
+
+        public T[] ToArray()
+        {
+            return this.CopyElements(new T[this.Count]);
+        }
+
+        public T Dequeue()
+        {
+            if(this.elements.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var head = this.elements[startIndex];
+
+            if(this.Count == 1)
+            {
+                this.elements[startIndex] = this.elements[endIndex] = default;
+                this.Count = 0;
+
+                return head;
+            }
+            if(startIndex == this.elements.Length - 1)
+            {
+                startIndex = 0;
+            }
+
+            startIndex = (startIndex + 1) % this.elements.Length;
+            this.Count--;
+            return head;
+        }
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int currentIndex = 0; currentIndex < this.Count; currentIndex++)
+            {
+                var index = (this.startIndex + currentIndex) % this.elements.Length;
+                yield return this.elements[index];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void Grow()
+        {
+            this.elements = this.CopyElements(new T[this.elements.Length * 2]);
+            this.startIndex = 0;
+            this.endIndex = this.Count;
+        }
+
+        private T[] CopyElements(T[] resultArr)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                resultArr[i] = this.elements[(this.startIndex + i) % this.elements.Length];
+            }
+
+            return resultArr;
+        }
+    }
+}
---
+++ 03. Linear-Data-Structures/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,134 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private class Node
+        {
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+            public T Value { get; set; }
+            
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            var newNode = new Node(item);
+            if (this.head == null)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                this.head.Previous = newNode;
+                newNode.Next = this.head;
+                this.head = newNode;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            var newNode = new Node(item);
+
+            if (this.head == null)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = this.tail;
+                this.tail.Next = newNode;
+                this.tail = newNode;
+            }
+
+            this.Count++;
+        }
+
+        public T GetFirst()
+        {
+            if(head == null) { throw new InvalidOperationException(); }
+
+            return this.head.Value;
+        }
+
+        public T GetLast()
+        {
+            if (tail == null) { throw new InvalidOperationException(); }
+
+            return this.tail.Value;
+        }
+
+        public T RemoveFirst()
+        {
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var currentHead = this.head;
+
+            if(this.head.Next == null)
+            {
+                this.head = this.tail = null;
+            }
+            else
+            {
+                this.head = this.head.Next;
+                this.head.Previous = null;
+            }
+
+            this.Count--;
+            return currentHead.Value;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var currentHead = this.head;
+
+            if (this.head.Next == null)
+            {
+                this.head = null;
+            }
+            else
+            {
+                this.head = this.head.Previous;
+                this.head.Next = null;
+            }
+
+            this.Count--;
+            return currentHead.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
