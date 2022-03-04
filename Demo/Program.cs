9bbdaf48b2f92343a992617e08d881958a69f68c
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-04 10:13:17 +0200

    demo

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo
+{
+    using System;
+    using Tree;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new string[] { "9 17", "91 171", "29 127", "39 157", "95 171", "2 167", "19 127" };
+
+            var treeFactory = new IntegerTreeFactory();
+
+            var tree = treeFactory.CreateTreeFromStrings(input);
+        }
+    }
+}
