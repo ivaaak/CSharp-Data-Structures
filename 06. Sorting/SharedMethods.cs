2afe56d5f9772ce8039b6098b91f7ed71fbe7852
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-22 08:54:50 +0300

    Added sorting project

---
+++ 06. Sorting/SharedMethods.cs
@@ -0,0 +1,15 @@
+﻿namespace Sorting
+{
+    public class SharedMethods
+    {
+        // Quick methods which are shared between sorting algos
+        public static void exchange(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[m] = data[n];
+            data[n] = temporary;
+        }
+    }
+}
