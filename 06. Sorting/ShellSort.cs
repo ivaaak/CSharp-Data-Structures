8822d63ad2474e557c127bea62d82b88da379085
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-23 09:43:42 +0300

    Added shellsort

---
+++ 06. Sorting/ShellSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Sorting
+{
+    public static class ShellSort
+    {
+        public static void IntArrayShellSort(int[] data, int[] intervals)
+        {
+            int i, j, k, m;
+            int N = data.Length;
+
+            // The intervals for the shell sort must be sorted, ascending
+
+            for (k = intervals.Length - 1; k >= 0; k--)
+            {
+                int interval = intervals[k];
+                for (m = 0; m < interval; m++)
+                {
+                    for (j = m + interval; j < N; j += interval)
+                    {
+                        for (i = j; i >= interval && data[i] < data[i - interval]; i -= interval)
+                        {
+                            SharedMethods.exchange(data, i, i - interval);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
