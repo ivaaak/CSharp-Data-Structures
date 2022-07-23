76013188b6386c14dc923e1a64c7b4acb425e7fa
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-23 09:44:05 +0300

    Added SelectionSort

---
+++ 06. Sorting/SelectionSort.cs
@@ -0,0 +1,27 @@
+﻿namespace Sorting
+{
+    public static class SelectionSort
+    {
+        public static void SelectionSortMethod(int[] input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                var min = i;
+                for (var j = i + 1; j < input.Length; j++)
+                {
+                    if (input[min] > input[j])
+                    {
+                        min = j;
+                    }
+                }
+
+                if (min != i)
+                {
+                    var lowerValue = input[min];
+                    input[min] = input[i];
+                    input[i] = lowerValue;
+                }
+            }
+        }
+    }
+}
