c60e4ae4ad682b0260a22d54204a2573751cac63
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-24 10:09:19 +0300

    Added HeapSort

---
+++ 06. Sorting/HeapSort.cs
@@ -0,0 +1,44 @@
+﻿namespace Sorting
+{
+    public class HeapSort
+    {
+        public void Sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+        }
+
+        void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i; 
+            int l = 2 * i + 1; 
+            int r = 2 * i + 2; 
+
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                Heapify(arr, n, largest);
+            }
+        }
+    }
+}
