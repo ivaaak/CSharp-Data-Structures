e3a3a88a01e6d54b4af17e20a830ecad94314418
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-24 10:09:40 +0300

    Added InsertionSort

---
+++ 06. Sorting/InsertionSort.cs
@@ -0,0 +1,23 @@
+﻿namespace Sorting
+{
+    public static class InsertionSort
+    {
+        public static void InsertionSortMethod(int[] input)
+        {
+
+            for (int i = 0; i < input.Count(); i++)
+            {
+                var item = input[i];
+                var currentIndex = i;
+
+                while (currentIndex > 0 && input[currentIndex - 1] > item)
+                {
+                    input[currentIndex] = input[currentIndex - 1];
+                    currentIndex--;
+                }
+
+                input[currentIndex] = item;
+            }
+        }
+    }
+}
