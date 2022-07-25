09c6b0df50339928a0767bcbc8f85657330090ca
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-25 09:28:24 +0300

    Added InsertionSort which in- and outputs a list

---
+++ 06. Sorting/InsertionSortList.cs
@@ -0,0 +1,25 @@
+﻿namespace Sorting
+{
+    public static class InsertionSortList
+    {
+        public static List<int> InsertionSortList(this List<int> input)
+        {
+            var clonedList = new List<int>(input.Count);
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var item = input[i];
+                var currentIndex = i;
+
+                while (currentIndex > 0 && clonedList[currentIndex - 1] > item)
+                {
+                    currentIndex--;
+                }
+
+                clonedList.Insert(currentIndex, item);
+            }
+
+            return clonedList;
+        }
+    }
+}
